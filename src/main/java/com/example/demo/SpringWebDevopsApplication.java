724a865cf8555a27707b912b15686b9cc7d3e853
Melbin <melbin.cruz40@gmail.com>
2019-06-25 23:00:52 -0600

    First commit

---
+++ src/main/java/com/example/demo/SpringWebDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebDevopsApplication.class, args);
+	}
+
+}
